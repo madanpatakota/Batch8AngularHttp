8cb725f39a6d446b31b105fc36ce08a608b0f35c
Madan <madan.patakota@gmail.com>
2023-09-01 10:40:07 +0530

    http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Batch8AngularHttp';
+export class AppComponent implements OnInit {
+  constructor(private httpClient: HttpClient) {}
+
+
+  // jsonplacehoder -- server
+       
+
+
+
+
+  //   localserver 
+
+  ngOnInit(): void {
+    this.httpClient.get("https://jsonplaceholder.typicode.com/posts").subscribe((data)=>{
+        console.log("MY project having the data from jsonholder",data);
+    })
+
+    this.httpClient
+      .get('https://in.bookmyshow.com/api/explore/v1/discover/regions')
+      .subscribe((data) => {
+        console.log('MY project having the data from bookmyshow', data);
+      });
+
+
+    this.httpClient
+      .get('https://api.firstamedu.com/api/api/state/GetAllStatesConfiguration')
+      .subscribe((data) => {
+        console.log('MY project having the data from Firstamdeud', data);
+      });
+
+      
+  }
+
+  //angular http
+
+  // to connect the apis
+
+  //https://jsonplaceholder.typicode.com/posts
 }
